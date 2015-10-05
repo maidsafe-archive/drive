3d04d72331240572e23b5b43fdac094d60e76cea
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-10-05 14:48:33 +0100

    Updated lint checks and README.md.

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # Drive
 
-**Primary Maintainer:**     Mahmoud Moadeli (mmoadeli@maidsafe.net)
-
-**Secondary Maintainer:**   Brian Smith (brian.smith@maidsafe.net)
+**Primary Maintainer:** Brian Smith (brian.smith@maidsafe.net)
 
 |Crate|Linux/OS X|Windows|Coverage|Issues|
 |:---:|:--------:|:-----:|:------:|:----:|
 |[![](http://meritbadge.herokuapp.com/drive)](https://crates.io/crates/drive)|[![Build Status](https://travis-ci.org/maidsafe/drive.svg?branch=master)](https://travis-ci.org/maidsafe/drive)|[![Build status](https://ci.appveyor.com/api/projects/status/so3q2w6g8vey2avl/branch/master?svg=true)](https://ci.appveyor.com/project/MaidSafe-QA/drive/branch/master)|[![Coverage Status](https://coveralls.io/repos/maidsafe/drive/badge.svg)](https://coveralls.io/r/maidsafe/drive)|[![Stories in Ready](https://badge.waffle.io/maidsafe/drive.png?label=ready&title=Ready)](https://waffle.io/maidsafe/drive)|
 
-| [API Documentation - master branch](http://maidsafe.net/drive/master) | [SAFE Network System Documention](http://systemdocs.maidsafe.net) | [MaidSafe website](http://maidsafe.net) | [Safe Community site](https://forum.safenetwork.io) |
+| [API Documentation - master branch](http://maidsafe.net/drive/master) | [SAFE Network System Documentation](http://systemdocs.maidsafe.net) | [MaidSafe website](http://maidsafe.net) | [SAFE Network Forum](https://forum.safenetwork.io) |
 |:------:|:-------:|:-------:|:-------:|
 
-#Overview
-
+## Overview
 
 A cross platform [virtual file-system in userspace](http://en.wikipedia.org/wiki/Filesystem_in_Userspace) (drive) that will appear as a regular drive on the operating system. The interface is a POSIX-like API and this is exposed in every OS. May include a webdav interface where possible.
 
@@ -20,26 +17,25 @@ IOS and Android…etc… may require a driverless option, further consideration 
 
 This drive can provide a blocking call to be used as a stand alone application, or a threaded call to enable a drive to be mounted from any application.
 
-#Build Prerequisites
+## Prerequisites
 
-##Linux
+### Linux
 
 Requires fuse dev files in ubuntu `sudo apt-get install libfuse-dev`
 
-##OSX
+### OS X
 
 Requires osxfuse (easiest method is to use Homebrew and `brew install osxfuse`
 
-##Bsd
+### BSD
 
 Likely working [Puffs](http://www.netbsd.org/docs/puffs/) and will require fuse-development library installed, but requires tests and CI integration.
 
-##Windows
+### Windows
 
-Currently unimplemented and will require the [windows driver frameworks](https://github.com/Microsoft/Windows-Driver-Frameworks) as  per these [examples](https://github.com/Microsoft/windows-driver-samples)
+Currently unimplemented and will require the [windows driver frameworks](https://github.com/Microsoft/Windows-Driver-Frameworks) as per these [examples](https://github.com/Microsoft/windows-driver-samples)
 
-
-#Todo
+## Todo Items
 - [ ] Finalise API
 - [ ] Confirm Bsd
 - [ ] Provide simple example (mirror)
--- src/lib.rs
+++ src/lib.rs
@@ -15,25 +15,32 @@
 // Please review the Licences for the specific language governing permissions and limitations
 // relating to use of the SAFE Network Software.                                                                 */
 
-#![crate_name = "drive"]
-#![crate_type = "lib"]
-#![forbid(bad_style, missing_docs, warnings)]
-#![deny(deprecated, drop_with_repr_extern, improper_ctypes, non_shorthand_field_patterns,
-        overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
-        raw_pointer_derive, stable_features, unconditional_recursion, unknown_lints, unsafe_code,
-        unused, unused_allocation, unused_attributes, unused_comparisons, unused_features,
-        unused_parens, while_true)]
-#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
-        unused_qualifications, unused_results, variant_size_differences)]
-#![doc(html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
-       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
-       html_root_url = "http://maidsafe.github.io/drive/")]
 //!#Drive
 //!A cross platform virtual file-system in userspace (drive) that will appear as a regular drive on the operating system.
 //! The interface is a POSIX-like API and this is exposed in every OS. May include a webdav interface where possible.
 //!IOS and Android etc. may require a driverless option and will require further consideration (webdav ?) to provide the
 //! same cross platform/OS compatibility.
 //!This drive can provide a blocking call to be used as a stand alone application or a threaded call to enable a drive to be mounted from any application.
+
+#![doc(html_logo_url =
+           "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
+       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
+       html_root_url = "http://maidsafe.github.io/drive/")]
+
+// For explanation of lint checks, run `rustc -W help` or see
+// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
+#![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
+          unknown_crate_types, warnings)]
+#![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, raw_pointer_derive, stable_features,
+        unconditional_recursion, unknown_lints, unsafe_code, unused, unused_allocation,
+        unused_attributes, unused_comparisons, unused_features, unused_parens, while_true)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
+        unused_qualifications, unused_results, variant_size_differences)]
+#![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
+         missing_debug_implementations)]
+
 #[test]
 fn dummy()  {
 }
